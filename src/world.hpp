a612e474933ca02691a8182f66fa17b7905d7935
Laurent Olivier <olivier.laurent@ensta-paris.fr>
2021-12-25 15:57:21 +0100

    overload world print operator

--- src/world.hpp
+++ src/world.hpp
@@ -10,7 +10,7 @@ public:
   vector<vec3> velocity;
 
 public:
-  explicit World(const int N=3)
+  explicit World(const int N = 3)
       : position(vector<vec3>(N, vec3())), velocity(vector<vec3>(N, vec3())){};
   explicit World(const int N, vector<vec3> position)
       : position(position), velocity(vector<vec3>(N, vec3())){};
@@ -18,4 +18,29 @@ public:
       : position(position), velocity(velocity){};
 
   vec3 force(const int objId) const;
+
+  /**
+   * @brief Overload of the operator << for the world
+   *
+   * @param os
+   * @return ostream&
+   */
+  friend inline ostream &operator<<(ostream &os, const World &world);
 };
+
+inline ostream &operator<<(ostream &os, const World &world) {
+  for (int i = 0; i < world.position.size(); i++) {
+    os << "Positions " << i << endl;
+    os << "[" << world.position[i].getX() << ", " << world.position[i].getY()
+       << ", " << world.position[i].getZ() << "]" << endl;
+  }
+  for (int i = 0; i < world.position.size(); i++) {
+    os << "Velocities " << i << endl;
+    os << "[" << world.velocity[i].getX() << ", " << world.velocity[i].getY()
+       << ", " << world.velocity[i].getZ() << "]";
+    if (i != world.position.size() - 1) {
+      os << endl;
+    }
+  }
+  return os;
+}
