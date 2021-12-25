80cf038ffe7d16147aadb969c546772e4837c6c3
Laurent Olivier <olivier.laurent@ensta-paris.fr>
2021-12-25 19:18:23 +0100

    simulation: stack->deque, add 2 random searches

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -2,26 +2,104 @@
 
 using namespace std;
 
-void Simulation::run(const int stepsNb) {
+void Simulation::run_example(const int &stepsNb) {
   vector<vec3> pos{vec3(1, 0, 0), vec3(0, 1, 0), vec3(0, 0, 1)};
   vector<vec3> vel{vec3(0, 0.7, 0), vec3(0, 0, 0.7), vec3(0.7, 0, 0)};
-  World world(3, pos, vel);
+  World world(pos, vel);
   for (int i = 0; i < stepsNb; i++) {
     this->log(world);
-    world = solver.step(world);
+    solver.step(world);
   }
   this->savePositions();
 }
 
-void Simulation::log(const World &world) {
-  this->positionHistory.push(world.position);
-  this->velocityHistory.push(world.velocity);
+void Simulation::run(const int &stepsNb, World &world) {
+  World cWorld(world);
+  for (int i = 0; i < stepsNb; i++) {
+    this->log(cWorld);
+    solver.step(cWorld);
+  }
 }
 
-void Simulation::savePositions(const string filename /*= "positions.txt"*/) {
+void Simulation::randomSearch(const int &searchNb) {
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_real_distribution<double> dis(0.0, 1.0);
+  for (int i = 0; i < searchNb; i++) {
+    vector<vec3> pos({vec3(dis, gen), vec3(dis, gen), vec3(dis, gen)});
+    vector<vec3> vel({vec3(dis, gen), vec3(dis, gen), vec3(dis, gen)});
+    World cWorld = World(pos, vel);
+
+    this->run(5000, cWorld);
+    double stability = this->stability();
+    if (stability < 0.1) {
+      cout << cWorld << endl << "Stability: " << stability << endl;
+    }
+  }
+}
+
+void Simulation::randomSearch() {
+  double minStability = 10.0;
+  bool found = false;
+
+  int i = 0;
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_real_distribution<double> dis(0.0, 1.0);
+  while (!found) {
+    vector<vec3> pos({vec3(dis, gen), vec3(dis, gen), vec3(dis, gen)});
+    vector<vec3> vel({vec3(dis, gen), vec3(dis, gen), vec3(dis, gen)});
+    World cWorld = World(pos, vel);
+
+    this->run(5000, cWorld);
+    double stability = this->stability();
+    if (stability < minStability) {
+      minStability = stability;
+      cout << " Min stability: " << minStability << endl << cWorld << endl;
+    }
+    if (i % 1000 == 0)
+      cout << i << " Min stability: " << minStability << endl;
+    ;
+    i++;
+    if (stability < 1) {
+      cout << cWorld << endl << "Stability: " << stability << endl;
+      found = true;
+    }
+    this->clear();
+  }
+}
+
+// double Simulation::stability() const {
+//   World init = this->worldHistory.front();
+//   World minWorld = *min(this->worldHistory.begin(), this->worldHistory.end(),
+//                         [init](const World w1, const World w2) {
+//                           return (init.distance(w1) < init.distance(w2));
+//                         });
+//   return init.distance(minWorld);
+// }
+
+double Simulation::stability() const {
+  World init(this->worldHistory.front());
+  double dist = init.distance(this->worldHistory.back());
+  for (int i = 1000; i < this->worldHistory.size(); i++) {
+    double newDist = init.distance(this->worldHistory[i]);
+    if (newDist < dist) {
+      dist = newDist;
+    }
+  }
+  return dist;
+}
+
+void Simulation::log(const World &world) {
+  this->positionHistory.push_back(world.position);
+  this->velocityHistory.push_back(world.velocity);
+  this->worldHistory.push_back(world);
+}
+
+void Simulation::savePositions(const string &filename /*= "positions.txt"*/) {
   ofstream outFile(filename);
   while (!this->positionHistory.empty()) {
-    vector<vec3> pos = this->positionHistory.top();
+    vector<vec3> pos = this->positionHistory.front();
     outFile << "[";
     for (int i = 0; i < this->N; i++) {
       outFile << pos[i];
@@ -30,16 +108,16 @@ void Simulation::savePositions(const string filename /*= "positions.txt"*/) {
       }
     }
     outFile << "]" << endl;
-    this->positionHistory.pop();
+    this->positionHistory.pop_front();
   }
 }
 
-void Simulation::save(const string filename /*= "data.txt"*/) {
+void Simulation::save(const string &filename /*= "data.txt"*/) {
   ofstream outFile(filename);
 
   outFile << "Positions:" << endl << "---" << endl;
   while (!this->positionHistory.empty()) {
-    vector<vec3> pos = this->positionHistory.top();
+    vector<vec3> pos = this->positionHistory.front();
     outFile << "[";
     for (int i = 0; i < this->N; i++) {
       outFile << pos[i];
@@ -48,12 +126,12 @@ void Simulation::save(const string filename /*= "data.txt"*/) {
       }
     }
     outFile << "]" << endl;
-    this->positionHistory.pop();
+    this->positionHistory.pop_front();
   }
 
   outFile << "---" << endl << "Velocities:" << endl << "---" << endl;
   while (!this->velocityHistory.empty()) {
-    vector<vec3> vel = this->velocityHistory.top();
+    vector<vec3> vel = this->velocityHistory.front();
     outFile << "[";
     for (int i = 0; i < this->N; i++) {
       outFile << vel[i];
@@ -62,6 +140,12 @@ void Simulation::save(const string filename /*= "data.txt"*/) {
       }
     }
     outFile << "]" << endl;
-    this->velocityHistory.pop();
+    this->velocityHistory.pop_front();
   }
+}
+
+void Simulation::clear() {
+  this->positionHistory.clear();
+  this->velocityHistory.clear();
+  this->worldHistory.clear();
 }
--- src/simulation.hpp
+++ src/simulation.hpp
@@ -1,31 +1,41 @@
 #pragma once
 #include "solver.hpp"
 #include "world.hpp"
+#include <algorithm>
+#include <deque>
 #include <fstream>
-#include <stack>
+#include <random>
 
 using namespace std;
+
 class Simulation {
 protected:
   int N;
-  stack<vector<vec3>> positionHistory;
-  stack<vector<vec3>> velocityHistory;
+  deque<vector<vec3>> positionHistory;
+  deque<vector<vec3>> velocityHistory;
+  deque<World> worldHistory;
   EulerSolver solver;
 
 public:
   explicit Simulation()
-      : N(), solver(EulerSolver()), positionHistory(stack<vector<vec3>>()),
-        velocityHistory(stack<vector<vec3>>()) {}
+      : N(), solver(EulerSolver()), positionHistory(deque<vector<vec3>>()),
+        velocityHistory(deque<vector<vec3>>()), worldHistory(deque<World>()) {}
 
-  explicit Simulation(int N, double h)
-      : N(N), solver(EulerSolver(N, h)), positionHistory(stack<vector<vec3>>()),
-        velocityHistory(stack<vector<vec3>>()) {}
+  explicit Simulation(const int &N, const double &h)
+      : N(N), solver(EulerSolver(N, h)), positionHistory(deque<vector<vec3>>()),
+        velocityHistory(deque<vector<vec3>>()), worldHistory(deque<World>()) {}
 
-  void run(const int stepsNb);
+  void run_example(const int &stepsNb);
+
+  void run(const int &stepsNb, World &world);
+
+  void randomSearch(const int &searchNb);
+  void randomSearch();
+
+  double stability() const;
 
   void log(const World &world);
-
-  void savePositions(const string filename = "positions.txt");
-
-  void save(const string filename = "data.txt");
+  void savePositions(const string &filename = "positions.txt");
+  void save(const string &filename = "data.txt");
+  void clear();
 };
